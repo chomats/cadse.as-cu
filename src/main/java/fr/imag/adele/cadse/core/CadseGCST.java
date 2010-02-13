6ed89e9269bbbe556a1df9d83fcba36e2984cae7
chomats <chomats@localhost>
2010-02-13 11:42:29 +0100

    remove file name field

--- src/main/java/fr/imag/adele/cadse/core/CadseGCST.java
+++ src/main/java/fr/imag/adele/cadse/core/CadseGCST.java
@@ -1,4 +1,3 @@
-/** Licensed to the Apache Software Foundation (ASF) under one* or more contributor license agreements.  See the NOTICE file* distributed with this work for additional information* regarding copyright ownership.  The ASF licenses this file* to you under the Apache License, Version 2.0 (the* "License"); you may not use this file except in compliance* with the License.  You may obtain a copy of the License at**   http://www.apache.org/licenses/LICENSE-2.0** Unless required by applicable law or agreed to in writing,* software distributed under the License is distributed on an* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY* KIND, either express or implied.  See the License for the* specific language governing permissions and limitations* under the License.*/
 package fr.imag.adele.cadse.core;
 
 
@@ -833,14 +832,6 @@ public class CadseGCST {
 	    @generated
 	*/
 	public static ItemType FILE_CONTENT_MODEL;
-	/**
-	    @generated
-	*/
-	public final static String FILE_CONTENT_MODEL_at_FILE_NAME="file-name";
-	/**
-	    @generated
-	*/
-	public static StringAttributeType FILE_CONTENT_MODEL_at_FILE_NAME_;
 	/**
 	    @generated
 	*/
@@ -2237,4 +2228,5 @@ public class CadseGCST {
 	}
 
 }
+/** Licensed to the Apache Software Foundation (ASF) under one* or more contributor license agreements.  See the NOTICE file* distributed with this work for additional information* regarding copyright ownership.  The ASF licenses this file* to you under the Apache License, Version 2.0 (the* "License"); you may not use this file except in compliance* with the License.  You may obtain a copy of the License at**   http://www.apache.org/licenses/LICENSE-2.0** Unless required by applicable law or agreed to in writing,* software distributed under the License is distributed on an* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY* KIND, either express or implied.  See the License for the* specific language governing permissions and limitations* under the License.*/
 
