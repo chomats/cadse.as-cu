df9977ecf79323d162c93831995ee629c3a02d1e
chomats <chomats@localhost>
2010-05-19 16:24:44 +0200

    fix validation

--- src/main/java/fr/imag/adele/cadse/core/AdaptableObject.java
+++ src/main/java/fr/imag/adele/cadse/core/AdaptableObject.java
@@ -12,4 +12,10 @@ package fr.imag.adele.cadse.core;
 public interface AdaptableObject {
 
 	public <T> T adapt(Class<T> clazz);
+	
+	public <T> T[] adapts(Class<T> clazz);
+	
+	public <T> void addAdater(ObjectAdapter<T> adapt);
+	
+	public <T> void removeAdater(ObjectAdapter<T> adapt);
 }
