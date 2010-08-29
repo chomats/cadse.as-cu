5bf981f233092ca3fd83ad28ba00eab31f7ce463
Stephane Chomat <Stephane.chomat@imag.fr>
2010-08-29 22:41:44 +0200

    fix

--- src/main/java/fr/imag/adele/cadse/core/CadseGCST.java
+++ src/main/java/fr/imag/adele/cadse/core/CadseGCST.java
@@ -1,6 +1,7 @@
 package fr.imag.adele.cadse.core;
 
 
+import fr.imag.adele.cadse.core.CadseRuntime;
 import fr.imag.adele.cadse.core.ExtendedType;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.LinkType;
@@ -2314,13 +2315,12 @@ public class CadseGCST {
 	*/
 	public static LinkType WCLISTENER_lt_LISTEN_ITEM_TYPES;
 	
-	public static UUID _CADSE_ID = java.util.UUID.fromString("2b50360d-d835-4e98-80a8-bc55fe5421e7");
+	public static UUID _CADSE_ID = new java.util.UUID(3121053974857600664L,-9175877163242216985L);
 
 	/**
-	    @generated
+		@generated
 	*/
-	public CadseGCST() {
-	}
+	public static CadseRuntime _CADSE;
 
 }
 /** Licensed to the Apache Software Foundation (ASF) under one* or more contributor license agreements.  See the NOTICE file* distributed with this work for additional information* regarding copyright ownership.  The ASF licenses this file* to you under the Apache License, Version 2.0 (the* "License"); you may not use this file except in compliance* with the License.  You may obtain a copy of the License at**   http://www.apache.org/licenses/LICENSE-2.0** Unless required by applicable law or agreed to in writing,* software distributed under the License is distributed on an* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY* KIND, either express or implied.  See the License for the* specific language governing permissions and limitations* under the License.*/
